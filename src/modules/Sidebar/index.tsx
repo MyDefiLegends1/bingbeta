a148e85c4e6a65ec1288dadc2b49311e1a8c1e88
MyDefiLegends1 <kalokohanofficial@gmail.com>
2023-05-09 13:19:56 +0800

    up

--- src/modules/Sidebar/index.tsx
+++ src/modules/Sidebar/index.tsx
@@ -8,7 +8,6 @@ import './index.css';
 import ImportConversationModal from '@components/ConversationModal/import';
 import RecordCard from './RecordCard';
 
-
 const Sidebar: FC<{
   data: RecordCardItem[];
 }> = ({ data }) => {
@@ -83,14 +82,13 @@ const Sidebar: FC<{
     <div className="flex flex-col h-full bg-white">
       <div className="p-4 mt-2 flex items-baseline justify-between">
         <span className="text-3xl text-gradient font-[800]">BingGPT</span>
-<a
+        <a
   href="https://t.me/BingBlongAI"
   target="_blank"
   rel="noreferrer"
 >
   <i className="ml-2 ri-github-fill text-xl" />
 </a>
-
       </div>
       <div className="p-2 flex items-center justify-between mb-4">
         <div className="rounded-xl h-10 border flex-1">
